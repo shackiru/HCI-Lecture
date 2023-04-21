40b2dac1f7eea27496498cb3b59e1eb9ef844684
alexanderis00 <82192504+alexanderis00@users.noreply.github.com>
2023-04-21 20:23:12 +0700

    u

--- shop/script-shop.js
+++ shop/script-shop.js
@@ -68,12 +68,152 @@ category3.onclick = function(){
     category3.style.transition = "background-color 0.5s ease, color 0.5s ease";
 }
 
-cart1.onmouseover = function(){
+yellowcart1.onmouseover = function(){
     cart1.style.opacity = "0";
     yellowcart1.style.opacity = "100";
 }
 
-cart1.onmouseout = function(){
+yellowcart1.onmouseout = function(){
     cart1.style.opacity = "100";
     yellowcart1.style.opacity = "0";
+}
+
+yellowcart2.onmouseover = function(){
+    cart2.style.opacity = "0";
+    yellowcart2.style.opacity = "100";
+}
+
+yellowcart2.onmouseout = function(){
+    cart2.style.opacity = "100";
+    yellowcart2.style.opacity = "0";
+}
+
+yellowcart3.onmouseover = function(){
+    cart3.style.opacity = "0";
+    yellowcart3.style.opacity = "100";
+}
+
+yellowcart3.onmouseout = function(){
+    cart3.style.opacity = "100";
+    yellowcart3.style.opacity = "0";
+}
+
+yellowcart4.onmouseover = function(){
+    cart4.style.opacity = "0";
+    yellowcart4.style.opacity = "100";
+}
+
+yellowcart4.onmouseout = function(){
+    cart4.style.opacity = "100";
+    yellowcart4.style.opacity = "0";
+}
+
+yellowcart5.onmouseover = function(){
+    cart5.style.opacity = "0";
+    yellowcart5.style.opacity = "100";
+}
+
+yellowcart5.onmouseout = function(){
+    cart5.style.opacity = "100";
+    yellowcart5.style.opacity = "0";
+}
+
+yellowcart6.onmouseover = function(){
+    cart6.style.opacity = "0";
+    yellowcart6.style.opacity = "100";
+}
+
+yellowcart6.onmouseout = function(){
+    cart6.style.opacity = "100";
+    yellowcart6.style.opacity = "0";
+}
+
+yellowcart7.onmouseover = function(){
+    cart7.style.opacity = "0";
+    yellowcart7.style.opacity = "100";
+}
+
+yellowcart7.onmouseout = function(){
+    cart7.style.opacity = "100";
+    yellowcart7.style.opacity = "0";
+}
+
+yellowcart8.onmouseover = function(){
+    cart8.style.opacity = "0";
+    yellowcart8.style.opacity = "100";
+}
+
+yellowcart8.onmouseout = function(){
+    cart8.style.opacity = "100";
+    yellowcart8.style.opacity = "0";
+}
+
+yellowcart9.onmouseover = function(){
+    cart9.style.opacity = "0";
+    yellowcart9.style.opacity = "100";
+}
+
+yellowcart9.onmouseout = function(){
+    cart9.style.opacity = "100";
+    yellowcart9.style.opacity = "0";
+}
+
+yellowcart10.onmouseover = function(){
+    cart10.style.opacity = "0";
+    yellowcart10.style.opacity = "100";
+}
+
+yellowcart10.onmouseout = function(){
+    cart10.style.opacity = "100";
+    yellowcart10.style.opacity = "0";
+}
+
+yellowcart11.onmouseover = function(){
+    cart11.style.opacity = "0";
+    yellowcart11.style.opacity = "100";
+}
+
+yellowcart11.onmouseout = function(){
+    cart11.style.opacity = "100";
+    yellowcart11.style.opacity = "0";
+}
+
+yellowcart12.onmouseover = function(){
+    cart12.style.opacity = "0";
+    yellowcart12.style.opacity = "100";
+}
+
+yellowcart12.onmouseout = function(){
+    cart12.style.opacity = "100";
+    yellowcart12.style.opacity = "0";
+}
+
+yellowcart13.onmouseover = function(){
+    cart13.style.opacity = "0";
+    yellowcart13.style.opacity = "100";
+}
+
+yellowcart13.onmouseout = function(){
+    cart13.style.opacity = "100";
+    yellowcart13.style.opacity = "0";
+}
+
+yellowcart14.onmouseover = function(){
+    cart14.style.opacity = "0";
+    yellowcart14.style.opacity = "100";
+}
+
+yellowcart14.onmouseout = function(){
+    cart14.style.opacity = "100";
+    yellowcart14.style.opacity = "0";
+}
+
+yellowcart15.onmouseover = function(){
+    cart15.style.opacity = "0";
+    yellowcart15.style.opacity = "100";
+}
+
+yellowcart15.onmouseout = function(){
+    cart15.style.opacity = "100";
+    yellowcart15.style.opacity = "0";
 }
